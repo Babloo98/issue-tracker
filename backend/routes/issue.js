3ae7f52b94042ab54a9423d00b1fc475ac18fba3
Ashu <ashu.sharma@tothenew.com>
2019-11-19 12:29:06 +0530

    apis created

--- backend/routes/issue.js
+++ backend/routes/issue.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const route = express.Router();
-const issue = require('./schema/issueSchema')
+const issue = require('./schema')
 route.get('/',(req,res)=>{
-    issue.find({ phone:req.body.phone })
-    .then((res)=>{
-        res.status(200).send(res);
+    console.log("rrrrrrrrr",req.body);
+    issue.find()
+    .then((resp)=>{
+        res.send(resp);
     });
+});
+route.post('/',(req,res)=>{
+    var data= new issue({type: "Glass Broken", phone: "99999999"});
+    data.save()
+    .then((ress)=>{
+        console.log(ress);
+    })
 });
 
 module.exports= route;
--- backend/routes/schema.js
+++ backend/routes/schema.js
@@ -2,17 +2,17 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  name: String,
-  phone: String,
-  email: String,
-  address: String,
-});
+// var userSchema = new Schema({
+//   name: String,
+//   phone: String,
+//   email: String,
+//   address: String,
+// });
 
 var issueSchema = new Schema({
     phone: String,
     type: String,
 })
 
-module.exports = mongoose.model("userSchema",userSchema);
+// module.exports = mongoose.model("userSchema",userSchema);
 module.exports = mongoose.model("issueSchema",issueSchema);
