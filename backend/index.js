876f62e663c1073c55d4683b573d7573c0e9cdf8
Ashu <ashu.sharma@tothenew.com>
2019-11-19 12:54:55 +0530

    apis created

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const login = require('./routes/login');
 const issue = require('./routes/issue.js');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const PORT = 8000;
 const app = express();
 
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 mongoose.connect(`mongodb://localhost/issue_tracker`);
