fafe03f6e934aef94532ca379f8d1ff6cb90e3ba
Babloo Mishra <babloo.mishra@tothenew.com>
2019-11-19 12:31:13 +0530

    sffsd

--- backend/routes/issue.js
+++ backend/routes/issue.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const route = express.Router();
-const issue = require('./schema/issueSchema')
+const issue = require('./schema')
 route.get('/',(req,res)=>{
-    issue.find({ phone:req.body.phone })
-    .then((res)=>{
-        res.status(200).send(res);
+    issue.find({  })
+    .then((resp)=>{
+        res.status(200).send(resp);
     });
 });
 
