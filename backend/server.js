b8f401778b1e646d962308a2a464f66555f67f23
Babloo Mishra <babloo.mishra@tothenew.com>
2020-01-04 18:27:53 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,23 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
     next();
   });
-mongoose.connect(`mongodb://localhost/issue_tracker`);
+
+
+const db = process.env.MONGODB_URL;
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(db, {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true
+//     });
+//     console.log("MongoDB is Connected...");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+mongoose.connect(`mongodb://localhost/issue_tracker` || db);
 
 app.use('/login',login);
 app.use('/issue',issue);
