4baf827ee0fe5bff2528c11669a1b8d2ad91472e
Babloo Mishra <babloo.mishra@tothenew.com>
2020-01-06 16:03:42 +0530

    icon added

--- backend/client/src/components/reset/index.js
+++ backend/client/src/components/reset/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import firebase from 'firebase';
 import axios from 'axios';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import Logo from '../../assets/logo.png';
+
 
 class Reset extends React.Component{
 
@@ -37,7 +39,7 @@ class Reset extends React.Component{
                 showCancelButton: false,
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
+                confirmButtonText: 'Yes, login!!'
               }).then((result) => {
                 if (result.value) {
                     self.props.history.push("/")
@@ -53,9 +55,11 @@ class Reset extends React.Component{
     render(){
         return(
             <div className="resetpasswordwrapper">
+                <img style= {{margin:"20px"}} src = {Logo} alt="logo"></img>
                 <div className="input-fields">
-                    <p>Enter Phone Number:</p>
+                    <p>Enter Email:</p>
                     <input type="email" name="email" value = {this.state.email} onChange = {this.handleChange} className="form-control" id="input1" placeholder="Enter Email" required/>
+                    <p>Enter new password</p>
                     <input type="password" name="password" value = {this.state.password} onChange = {this.handleChange} className="form-control" id="input2" placeholder="Enter Password" required/>
                     <button onClick = {this.onClickHandler} className = "btn btn-danger mt-5">Reset Password</button>
                 </div>
